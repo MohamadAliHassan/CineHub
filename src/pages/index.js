91631635044edab176453a1280e13a8a761f8374
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-08-03 18:28:09 +0200

    fixed the bug issue with imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { MovieList } from "./MovieList";
 export { MovieDetail } from "./MovieDetail";
 export { Search } from "./Search";
+
 export { PageNotFound } from "./PageNotFound";
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,2 +1,25 @@
+// AllRoutes.js
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { MovieList, MovieDetail, PageNotFound, Search } from "../pages";
+import MovieList from "../pages/MovieList";
+import MovieDetail from "../pages/MovieDetail";
+import PageNotFound from "../pages/PageNotFound";
+import Search from "../pages/Search";
+
+function AllRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/movies/popular" element={<MovieList />} />
+        <Route path="/movies/top" element={<MovieList />} />
+        <Route path="/movies/upcoming" element={<MovieList />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default AllRoutes;
