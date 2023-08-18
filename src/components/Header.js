a6a3afa0ef969e074fc01fdc9d85197309779479
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-08-18 10:25:30 +0200

    configured dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,8 @@ function Header() {
     "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 text-base";
   const nonActive =
     "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-base";
+
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <header>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -18,7 +20,43 @@ function Header() {
               CineHub
             </span>
           </Link>
-          <div className="flex md:order-2">
+
+          <div id="mobile-nav" className="flex md:order-2">
+            <button
+              className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
+              type="button"
+              data-toggle-dark="light"
+              onClick={() => setDarkMode(!darkMode)}
+            >
+              {darkMode ? (
+                <svg
+                  aria-hidden="true"
+                  data-toggle-icon="sun"
+                  class="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              ) : (
+                <svg
+                  aria-hidden="true"
+                  data-toggle-icon="moon"
+                  class="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              )}
+            </button>
             <button
               type="button"
               data-collapse-toggle="navbar-search"
@@ -32,6 +70,7 @@ function Header() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 20 20"
+                onClick={() => setHidden(!hidden)}
               >
                 <path
                   stroke="currentColor"
@@ -70,7 +109,9 @@ function Header() {
                 autoComplete="off"
               />
             </div>
+
             <button
+              onClick={() => setHidden(!hidden)}
               data-collapse-toggle="navbar-search"
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -95,9 +136,12 @@ function Header() {
               </svg>
             </button>
           </div>
+
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-search"
+            className={`items-center justify-between ${
+              hidden ? "hidden" : ""
+            } w-full md:flex md:w-auto md:order-1`} //1
+            id="navbar-links" //Subhams toturial är det nav-links
           >
             <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
