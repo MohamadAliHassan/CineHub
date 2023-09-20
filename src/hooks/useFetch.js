32a04420483aeab9c1ce4d35be31746243151fce
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-09-20 17:45:43 +0200

    fixed querySearch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function useFetch(apiPath, queryTerm = "") {
   const [data, setData] = useState([]);
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`;
+  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
   useEffect(() => {
     async function fetchMovies() {
       const response = await fetch(url);
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,13 +3,20 @@ import Card from "../components/Card";
 import useFetch from "../hooks/useFetch";
 import { useSearchParams } from "react-router-dom";
 
-function Search(apiPath) {
+function Search({ apiPath }) {
   const [searchParams] = useSearchParams();
   const queryTerm = searchParams.get("q");
   const { data: movies } = useFetch(apiPath, queryTerm);
   return (
     <main>
       <div>
+        <section>
+          <p className="text-3xl text-gray-600 dark:text-white">
+            {movies.length === 0
+              ? `No results for ${queryTerm}`
+              : `Results for ${queryTerm}`}
+          </p>
+        </section>
         <section className="max-w-7xl mx-auto py-7">
           <div className="flex justify-start flex-wrap">
             {/* Mappa/loop genom varje film som fetchas och extract every item*/}
